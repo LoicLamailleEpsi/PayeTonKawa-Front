0b4cc968c9a3ad193af7baf66d1dc4cf9c87edbb
alexlabuz <alex.labuz@live.fr>
2023-07-25 11:06:53 +0200

    Add coffee machine ar moving

--- lib/page/ar_page.dart
+++ lib/page/ar_page.dart
@@ -22,6 +22,7 @@ class _ArPageState extends State<ArPage> {
   ARNode? _webObjectNode;
 
   bool _loadGlb = false;
+  bool _nodeIsLoad = false;
 
   void onARViewCreated(
     ARSessionManager arSessionManager, 
@@ -39,8 +40,8 @@ class _ArPageState extends State<ArPage> {
       showWorldOrigin: false,
       showAnimatedGuide: false,
       handleTaps: false,
-      handleRotation: true,
-      handlePans: true,
+      handleRotation: false,
+      handlePans: false,
     );
 
     _arObjectManager.onInitialize();
@@ -57,40 +58,71 @@ class _ArPageState extends State<ArPage> {
       _webObjectNode = null;
     } else {
       var newNode = ARNode(
-          type: NodeType.webGLB,
-          uri: "https://raw.githubusercontent.com/LoicLamailleEpsi/PayeTonKawa-Front/feat/coffeeAR/Nespresso.glb",
-          scale: Vector3(0.1, 0.1, 0.1));
+        type: NodeType.webGLB,
+        uri: "https://raw.githubusercontent.com/LoicLamailleEpsi/PayeTonKawa-Front/main/Nespresso.glb",
+        scale: Vector3(0.1, 0.1, 0.1),
+        position: Vector3(0, 0, 0)
+      );
       bool? didAddWebNode = await _arObjectManager.addNode(newNode);
       _webObjectNode = (didAddWebNode!) ? newNode : null;
     }
+    setState(() => _nodeIsLoad = true);
     loadGlb(false);
+  }
+
+  void moveNode(DragUpdateDetails swipe) {
+    var x = _webObjectNode!.position.x;
+    var y = _webObjectNode!.position.y;
+    var z = _webObjectNode!.position.z;
+
+    if(swipe.delta.dx > 1) x += 0.002;
+    if(swipe.delta.dx < -1) x -= 0.002;
+    if(swipe.delta.dy > 1) y -= 0.002;
+    if(swipe.delta.dy < -1) y += 0.002;
+
+    _webObjectNode!.position = Vector3(x, y, z);
+  }
+
+  void resetPositionNode(){
+    _webObjectNode!.position = Vector3(0, 0, 0);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("AR"),
+        title: const Text("Affichage en realité augmenté"),
       ),
       body: Column(
         children: [
           Expanded(
-            child: ARView(
-              onARViewCreated: onARViewCreated,
-              
+            child: GestureDetector(
+              onPanUpdate: (details) => moveNode(details),
+              child: ARView(
+                onARViewCreated: onARViewCreated,
+              ),
             ),
           ),
 
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
+            child: Column(
               children: [
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: !_loadGlb ? () => onWebObjectAtButtonPressed() : null, 
-                    child: _loadGlb ? const SizedBox(width: 20,  height: 20, child: CircularProgressIndicator()) : const Text("Afficher la machine à café"),
+                Visibility(
+                  visible: !_nodeIsLoad,
+                  replacement: SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () => resetPositionNode(), 
+                      child: const Text("Rénitialiser la position"),
+                    ),
+                  ),
+                  child: SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: !_loadGlb ? () => onWebObjectAtButtonPressed() : null, 
+                      child: _loadGlb ? const SizedBox(width: 20,  height: 20, child: CircularProgressIndicator()) : const Text("Afficher la machine à café"),
+                    ),
                   ),
                 ),
               ],
